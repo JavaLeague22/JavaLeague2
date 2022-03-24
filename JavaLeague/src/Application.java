146e75cec4e6ab697efa8084d0423aef988cea1c
sfrazier7 <sfrazier793@gmail.com>
2022-03-24 14:08:32 -0400

    Completed Methods add, info, list and count

--- JavaLeague/src/Application.java
+++ JavaLeague/src/Application.java
@@ -1,5 +1,8 @@
 
 
+import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
@@ -8,14 +11,59 @@ import java.util.Scanner;
 public class Application {
 	
 	static ArrayList<Contact> contactList = new ArrayList<Contact>();
+	static Scanner key = new Scanner(System.in);
 	
+	//add method
+	static void add() {
+		System.out.println("Do you want to add a person, or Organization. Enter 1 for Person, 2 for Organization.");
+		int choice = key.nextInt();
+		if(choice == 1) {
+			System.out.println("Enter the email.");
+			String email = key.nextLine();
+			System.out.println("Enter the contacts's date of birth in 'dd/mm/yyyy' format.");
+			String date = key.nextLine();
+			Date DOB = new SimpleDateFormat("dd/mm/yyyy").parse(date);
+			System.out.println("Enter the name.");
+			String name = key.nextLine();
+			System.out.println("Enter the contact number.");
+			String contactNumber = key.nextLine();
+			Person contact = new Person(email, DOB, name, contactNumber);
+			contactList.add(contact);
+		} else if(choice == 2){
+			System.out.println("Enter the name.");
+			String name = key.nextLine();
+			System.out.println("Enter the contact number.");
+			String number = key.nextLine();
+			System.out.println("Enter the website.");
+			String website = key.nextLine();
+			Organization contact = new Organization(name, number, website);
+			contactList.add(contact);
+		}
+	}
 	//list method
 	static void list() {
-		System.out.println(contactList);
+		int j = 0;
+		for(Contact i : contactList) {
+			System.out.println(contactList.indexOf(i) + ":" + contactList.get(j).name);
+		}
+	}
+	
+	//count method
+	static void count() {
+		System.out.println(contactList.size());
+	}
+	
+	//info method
+	static void info() {
+		Application.list();
+		int input = key.nextInt();
+		if(contactList.contains(contactList.get(input))) {
+			System.out.println(contactList.get(input));
+		}
 	}
 	
 	//search method
-	static Contact search() {
+	static void search() {
 		
 		//HashMap to store Contacts
 		HashMap<Integer, Contact> hashContactList = new HashMap<Integer, Contact>();
@@ -32,8 +80,8 @@ public class Application {
 		
 		//Loops through HashMap for equivalent names / and prints equivalent keys with matching names
 		for(int j = 0; j < hashContactList.size(); j++) {
-			if(query.equalsIgnoreCase(hashContactList.get(j).getName())); {
-				System.out.println(j + "." + hashContactList.get(j).getName()));
+			if(query.equalsIgnoreCase(hashContactList.get(j).name)); {
+				System.out.println(j + "." + hashContactList.get(j).name);
 			}
 		}
 		 
