9f1202de483848093027aaf7e8224c73e8b33ba6
Chelsea Cole <73906280+CcoleCode@users.noreply.github.com>
2022-03-24 11:21:26 -0400

    # WARNING: head commit changed in the meantime
    else if statements added

--- JavaLeague/src/Application.java
+++ JavaLeague/src/Application.java
@@ -10,6 +10,8 @@ public class Application {
 
 	public static void main(String[] args) {
 		
+		int userInput;
+		
 		Scanner input = new Scanner(System.in);
 		
 		ArrayList<String> Contacts = new ArrayList<String>();
@@ -21,9 +23,8 @@ public class Application {
 		infoMethod();
 		deleteMethod();
 		searchMethod();
-		exitMethod();
 		*/
-		//
+		
 		
 		System.out.println("Make a selection from the following menu");
 		
@@ -31,16 +32,34 @@ public class Application {
 		//display menu until the exit option is chosen
 			System.out.println("1.Add 2.List 3.Count 4.Info 5.Delete 6.Search 7.Exit");
 		
-		} while (input);
-
-			
-			try (FileNotFoundException a) {
-				
-				
-			} catch (FileNotFoundException a) {
-				System.out.println("No file exists");
+		} while (userInput <= 6);
 		
-			}
+		if (userInput == 1) {
+				//insert add method
+		} else if (userInput == 2) {
+				//insert list method
+		} else if (userInput == 3) {
+				//insert count method
+		} else if (userInput == 4) {
+				//insert info method
+		} else if (userInput == 5) {
+				//insert delete method
+		} else if (userInput == 6) {
+				//insert search method
+		} else if (userInput == 7) {
+			System.out.println("Thank you. Have a Nice Day!");
+		} else {
+			System.out.println("Invalid selection");
+		
+		}
+			
+			//try (FileNotFoundException a) {
+				
+				
+			//} catch (FileNotFoundException a) {
+			//	System.out.println("No file exists");
+		
+			
 			
 			input.close();
 }
